4744baa58cd30f4fc23aa1c9511bf8613376b9c6
Mizux Seiha <mizux.dev@gmail.com>
2023-02-10 00:52:35 +0100

    rogue: update deps

--- rogue/scripts/update_deps.sh
+++ rogue/scripts/update_deps.sh
@@ -6,6 +6,7 @@ command -v wget
 
 wget -N --trust-server-name https://unpkg.com/three/build/three.module.js
 #wget -N --trust-server-name https://raw.githubusercontent.com/mrdoob/three.js/master/examples/jsm/renderers/CSS3DRenderer.js
+#wget -N --trust-server-name https://raw.githubusercontent.com/mrdoob/three.js/master/examples/jsm/controls/OrbitControls.js
 
 wget -N --trust-server-name https://raw.githubusercontent.com/mrdoob/three.js/master/examples/jsm/libs/stats.module.js
 
@@ -13,4 +14,8 @@ wget -N --trust-server-name https://raw.githubusercontent.com/mrdoob/three.js/ma
 #wget -N --trust-server-name https://unpkg.com/dat.gui/build/dat.gui.module.js.map
 
 #wget -N --trust-server-name https://unpkg.com/gsap/dist/all.js
-wget -N --trust-server-name https://unpkg.com/gsap/dist/gsap.js
+#wget -N --trust-server-name https://unpkg.com/gsap/dist/gsap.js
+
+wget -N --trust-server-name https://unpkg.com/es6-tween/bundled/Tween.js
+
+wget -N --trust-server-name https://unpkg.com/rot-js/dist/rot.js
