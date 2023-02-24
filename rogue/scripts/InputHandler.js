cc4017a565a7a5b37c8024924d64bf2e67afe2b5
Corentin Le Molgat <corentinl@google.com>
2023-02-24 10:35:50 +0100

    rogue: Fix InputHandler Direction
    threejs use cartesian space with Y up

--- rogue/scripts/InputHandler.js
+++ rogue/scripts/InputHandler.js
@@ -11,14 +11,14 @@ export default class InputHandler {
     this.#game = game;
 
     this.#keyMap = {};
-    this.#keyMap[38] = ROT.DIRS[8][0]; // ArrowUp (8)
-    this.#keyMap[33] = ROT.DIRS[8][1]; // PageUp (9)
+    this.#keyMap[38] = ROT.DIRS[8][4]; // ArrowUp (8)
+    this.#keyMap[33] = ROT.DIRS[8][3]; // PageUp (9)
     this.#keyMap[39] = ROT.DIRS[8][2]; // ArrowRight (6)
-    this.#keyMap[34] = ROT.DIRS[8][3]; // PageDown (3)
-    this.#keyMap[40] = ROT.DIRS[8][4]; // ArrowDown (2)
-    this.#keyMap[35] = ROT.DIRS[8][5]; // End (1)
+    this.#keyMap[34] = ROT.DIRS[8][1]; // PageDown (3)
+    this.#keyMap[40] = ROT.DIRS[8][0]; // ArrowDown (2)
+    this.#keyMap[35] = ROT.DIRS[8][7]; // End (1)
     this.#keyMap[37] = ROT.DIRS[8][6]; // ArrowLeft (4)
-    this.#keyMap[36] = ROT.DIRS[8][7]; // Home (7)
+    this.#keyMap[36] = ROT.DIRS[8][5]; // Home (7)
     
     //keyMap[12] = ; // Clear (5)
     
