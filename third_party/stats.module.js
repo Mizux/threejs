d1780af24cc1fedbe0ebf1b72239e367ade75b8d
Corentin Le Molgat <corentinl@google.com>
2023-06-09 08:57:25 +0200

    third_party: cleanup

--- third_party/stats.module.js
+++ third_party/stats.module.js
@@ -10,11 +10,11 @@ class Stats {
   #memPanel;
 
   constructor() {
-    this.dom = document.createElement("div");
+    this.dom = document.createElement('div');
     this.dom.style.cssText =
-      "position:fixed;top:0;left:0;cursor:pointer;opacity:0.9;z-index:10000";
+      'position:fixed;top:0;left:0;cursor:pointer;opacity:0.9;z-index:10000';
     this.dom.addEventListener(
-      "click",
+      'click',
       (event) => {
         event.preventDefault();
         this.showPanel(++this.mode % this.dom.children.length);
@@ -22,15 +22,15 @@ class Stats {
       false
     );
 
-    this.#beginTime = (performance || Date).now()
-    this.#prevTime = this.#beginTime
+    this.#beginTime = (performance || Date).now();
+    this.#prevTime = this.#beginTime;
     this.#frames = 0;
 
-    this.#fpsPanel = this.addPanel(new Panel("FPS", "#0ff", "#002"));
-    this.#msPanel = this.addPanel(new Panel("MS", "#0f0", "#020"));
+    this.#fpsPanel = this.addPanel(new Panel('FPS', '#0ff', '#002'));
+    this.#msPanel = this.addPanel(new Panel('MS', '#0f0', '#020'));
 
     if (self.performance?.memory) {
-      this.#memPanel = this.addPanel(new Panel("MB", "#f08", "#201"));
+      this.#memPanel = this.addPanel(new Panel('MB', '#f08', '#201'));
     }
     this.showPanel(0);
   }
@@ -44,7 +44,7 @@ class Stats {
   /** @param id {number} */
   showPanel(id) {
     for (var i = 0; i < this.dom.children.length; i++) {
-      this.dom.children[i].style.display = i === id ? "block" : "none";
+      this.dom.children[i].style.display = i === id ? 'block' : 'none';
     }
     this.mode = id;
   }
@@ -58,7 +58,10 @@ class Stats {
     const time = (performance || Date).now();
     this.#msPanel.update(time - this.#beginTime, 200);
     if (time >= this.#prevTime + 1000) {
-      this.#fpsPanel.update((this.#frames * 1000) / (time - this.#prevTime), 100);
+      this.#fpsPanel.update(
+        (this.#frames * 1000) / (time - this.#prevTime),
+        100
+      );
       this.#prevTime = time;
       this.#frames = 0;
 
@@ -76,21 +79,21 @@ class Stats {
   update() {
     this.#beginTime = this.end();
   }
-};
+}
 
 class Panel {
   static PR = Math.round(window.devicePixelRatio || 1);
   static WIDTH = 80 * Panel.PR;
-  static HEIGHT = 48 * Panel.PR
+  static HEIGHT = 48 * Panel.PR;
   static TEXT_X = 3 * Panel.PR;
-  static TEXT_Y = 2 * Panel.PR
-  static GRAPH_X = 3 * Panel.PR
-  static GRAPH_Y = 15 * Panel.PR
-  static GRAPH_WIDTH = 74 * Panel.PR
+  static TEXT_Y = 2 * Panel.PR;
+  static GRAPH_X = 3 * Panel.PR;
+  static GRAPH_Y = 15 * Panel.PR;
+  static GRAPH_WIDTH = 74 * Panel.PR;
   static GRAPH_HEIGHT = 30 * Panel.PR;
 
-  #min = Infinity
-  #max = 0
+  #min = Infinity;
+  #max = 0;
 
   /**
    * @param name {string}
@@ -101,29 +104,40 @@ class Panel {
     this.fg = fg;
     this.bg = bg;
 
-    this.canvas = document.createElement("canvas");
+    this.canvas = document.createElement('canvas');
     this.canvas.width = Panel.WIDTH;
     this.canvas.height = Panel.HEIGHT;
-    this.canvas.style.cssText = "width:80px;height:48px";
+    this.canvas.style.cssText = 'width:80px;height:48px';
 
-    this.context = this.canvas.getContext("2d")
+    this.context = this.canvas.getContext('2d');
     if (this.context === null) {
-      console.error("Can't build context 2d!");
+      console.error('Can\'t build context 2d!');
       return;
     }
-    this.context.font = "bold " + 9 * Panel.PR + "px Helvetica,Arial,sans-serif";
-    this.context.textBaseline = "top";
+    this.context.font =
+      'bold ' + 9 * Panel.PR + 'px Helvetica,Arial,sans-serif';
+    this.context.textBaseline = 'top';
 
     this.context.fillStyle = this.bg;
     this.context.fillRect(0, 0, Panel.WIDTH, Panel.HEIGHT);
 
     this.context.fillStyle = this.fg;
     this.context.fillText(this.name, Panel.TEXT_X, Panel.TEXT_Y);
-    this.context.fillRect(Panel.GRAPH_X, Panel.GRAPH_Y, Panel.GRAPH_WIDTH, Panel.GRAPH_HEIGHT);
+    this.context.fillRect(
+      Panel.GRAPH_X,
+      Panel.GRAPH_Y,
+      Panel.GRAPH_WIDTH,
+      Panel.GRAPH_HEIGHT
+    );
 
     this.context.fillStyle = this.bg;
     this.context.globalAlpha = 0.9;
-    this.context.fillRect(Panel.GRAPH_X, Panel.GRAPH_Y, Panel.GRAPH_WIDTH, Panel.GRAPH_HEIGHT);
+    this.context.fillRect(
+      Panel.GRAPH_X,
+      Panel.GRAPH_Y,
+      Panel.GRAPH_WIDTH,
+      Panel.GRAPH_HEIGHT
+    );
   }
 
   /**
@@ -134,15 +148,20 @@ class Panel {
     this.#min = Math.min(this.#min, value);
     this.#max = Math.max(this.#max, value);
 
-    if (this.context === null)
-      return;
+    if (this.context === null) return;
     this.context.fillStyle = this.bg;
     this.context.globalAlpha = 1;
     this.context.fillRect(0, 0, Panel.WIDTH, Panel.GRAPH_Y);
     this.context.fillStyle = this.fg;
     this.context.fillText(
-      Math.round(value) + " " + this.name +
-      " (" + Math.round(this.#min) + "-" + Math.round(this.#max) + ")",
+      Math.round(value) +
+        ' ' +
+        this.name +
+        ' (' +
+        Math.round(this.#min) +
+        '-' +
+        Math.round(this.#max) +
+        ')',
       Panel.TEXT_X,
       Panel.TEXT_Y
     );
@@ -163,7 +182,8 @@ class Panel {
       Panel.GRAPH_X + Panel.GRAPH_WIDTH - Panel.PR,
       Panel.GRAPH_Y,
       Panel.PR,
-      Panel.GRAPH_HEIGHT);
+      Panel.GRAPH_HEIGHT
+    );
 
     this.context.fillStyle = this.bg;
     this.context.globalAlpha = 0.9;
@@ -174,6 +194,6 @@ class Panel {
       Math.round((1 - value / maxValue) * Panel.GRAPH_HEIGHT)
     );
   }
-};
+}
 
 export default Stats;
